207f4abcc2313303e7106924b5040d2bcdabf550
mehmetCt <mehmet@cybertekschool.com>
2024-05-10 15:13:54 -0400

    main --> master

--- src/test/java/cydeo/pages/PracticePage.java
+++ src/test/java/cydeo/pages/PracticePage.java
@@ -35,7 +35,7 @@ public class PracticePage {
     }
 
     public void login() {
-
+        // ENV VARIABLE ?
 
         String username = ConfigurationReader.getProperty("username");
         String password = ConfigurationReader.getProperty("password");
